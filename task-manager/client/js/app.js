670c6ba5c7302912ea6bdc048fdf45abe568914d
Vaishnaviverma09 <149881200+Vaishnaviverma09@users.noreply.github.com>
2025-07-16 23:58:31 +0530

    Delete task-manager/client/js directory

--- task-manager/client/js/app.js
+++
@@ -1,5 +0,0 @@
-// This file can be used for any additional app-wide functionality
-document.addEventListener('DOMContentLoaded', () => {
-  // Any initialization code that needs to run across the entire app
-  console.log('Task Manager 2025 initialized');
-});
